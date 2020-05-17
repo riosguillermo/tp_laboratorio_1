dca8af4cc595cb6423a118664e22195811fa9a6a
riosguillermo <60698031+riosguillermo@users.noreply.github.com>
2020-05-16 21:53:16 -0300

    Delete funciones.h

--- funciones.h
+++
@@ -1,7 +0,0 @@
-int getInt(char mensaje[]);
-float getFloat(char mensaje[]);
-float sumar(float A, float B);
-float restar(float A, float B);
-float dividir(int* pDivisionCero,float A, float B);
-float multiplicar(float A, float B);
-long int factorial(int* factorialError, float A);
